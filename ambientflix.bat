a37c3b8a6609c38018c4f37aac8e01db7651e1c6
Ed Bukoski <ebukoski@gmail.com>
2020-07-27 22:40:32 -0700

    first commit

---
+++ ambientflix.bat
@@ -0,0 +1,26 @@
+@echo on
+
+set DATA_COLLECTOR_HOME=c:\ambientflix\dataCollection
+set REC_ENGINE_HOME=c:\ambientflix\recEngine
+set WEB_CODE_HOME=c:\ambientflix\websiteCode
+
+echo "Gathering ambient data..."
+
+pushd %DATA_COLLECTOR_HOME%
+python dataCollection.py
+popd
+
+echo "Reving the REC ENGINE..."
+pushd %REC_ENGINE_HOME%
+gradlew bootRun -P%DATA_COLLECTOR_HOME%\collecteddata.txt > recs.txt
+popd
+
+echo "Todo....do we need to format the recs.txt file????"
+
+echo "Stunning web display is next..."
+pushd %WEB_CODE_HOME%
+start gradlew run -P%REC_ENGINE_HOME%\recs.txt
+popd
+
+echo "Launching page..."
+start http://localhost:8080/recs/
