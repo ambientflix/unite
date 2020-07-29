bd50da388cd202812ab8b765ced99d0d7eb395c6
ebukoski <ebukoski@netflix.com>
2020-07-28 18:59:19 -0700

    Update ambientflix.bat
    updates from meeting today

--- ambientflix.bat
+++ ambientflix.bat
@@ -4,21 +4,23 @@ set DATA_COLLECTOR_HOME=c:\ambientflix\dataCollection
 set REC_ENGINE_HOME=c:\ambientflix\recEngine
 set WEB_CODE_HOME=c:\ambientflix\websiteCode
 
+echo "Usage: ambientflix.bat [location] [twitter_handle]"
+
 echo "Gathering ambient data..."
 pushd %DATA_COLLECTOR_HOME%
-python dataCollection.py
+python dataCollection.py %1 %2
 popd
 
 echo "Reving the REC ENGINE..."
 pushd %REC_ENGINE_HOME%
-gradlew bootRun -P%DATA_COLLECTOR_HOME%\outputFile.txt > recs.txt
+gradlew bootRun --args=%DATA_COLLECTOR_HOME%\outputFile.txt 
 popd
 
 echo "Todo....do we need to format the recs.txt file????"
 
 echo "Stunning web display is next..."
 pushd %WEB_CODE_HOME%
-start gradlew run -P%REC_ENGINE_HOME%\recs.txt
+start gradlew run --args=%REC_ENGINE_HOME%\recList.txt
 popd
 
 echo "Launching page..."
